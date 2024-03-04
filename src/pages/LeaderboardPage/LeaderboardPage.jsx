d045776de1017ed52d869fedfd9347542de737ba
svet-nikol <plaksina.sn@mail.ru>
2024-03-04 21:06:17 +0300

    лоудер на время загрузки списка лидеров

--- src/pages/LeaderboardPage/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage/LeaderboardPage.jsx
@@ -7,12 +7,15 @@ import { formatTime } from "../../utils/formatTime";
 
 export function LeaderboardPage() {
   const { leadersList, setLeadersList } = useLeaders();
+  const [leadersListLoading, setLeadersListLoading] = useState(false);
 
   const [getLeadersError, setGetLeadersError] = useState(null);
   useEffect(() => {
+    setLeadersListLoading(true);
     getLeadersApi()
       .then(data => {
         setLeadersList(data.leaders);
+        setLeadersListLoading(false);
       })
       .catch(error => {
         setGetLeadersError(error.message);
@@ -23,35 +26,41 @@ export function LeaderboardPage() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <p>Лидерборд</p>
-        <Button to="/">Начать игру</Button>
-      </div>
+      {leadersListLoading ? (
+        <div className={styles.loader}></div>
+      ) : (
+        <>
+          <div className={styles.header}>
+            <p>Лидерборд</p>
+            <Button to="/">Начать игру</Button>
+          </div>
 
-      <div className={styles.listWrap}>
-        {getLeadersError ? (
-          <div className={styles.errorMessage}>{getLeadersError}</div>
-        ) : (
-          <>
-            <div className={styles.listItem} style={{ color: "#999999" }}>
-              <p className={styles.ItemPosition}>Позиция</p>
-              <p className={styles.ItemUser}>Пользователь</p>
-              <p className={styles.ItemAchiev}></p>
-              <p className={styles.ItemTime}>Время</p>
-            </div>
-
-            {leadersList &&
-              sortedLeadersList.map((item, index) => (
-                <div className={styles.listItem} key={item.id}>
-                  <p className={styles.ItemPosition}># {index + 1}</p>
-                  <p className={styles.ItemUser}>{item.name}</p>
+          <div className={styles.listWrap}>
+            {getLeadersError ? (
+              <div className={styles.errorMessage}>{getLeadersError}</div>
+            ) : (
+              <>
+                <div className={styles.listItem} style={{ color: "#999999" }}>
+                  <p className={styles.ItemPosition}>Позиция</p>
+                  <p className={styles.ItemUser}>Пользователь</p>
                   <p className={styles.ItemAchiev}></p>
-                  <p className={styles.ItemTime}>{formatTime(item.time)}</p>{" "}
+                  <p className={styles.ItemTime}>Время</p>
                 </div>
-              ))}
-          </>
-        )}
-      </div>
+
+                {leadersList &&
+                  sortedLeadersList.map((item, index) => (
+                    <div className={styles.listItem} key={item.id}>
+                      <p className={styles.ItemPosition}># {index + 1}</p>
+                      <p className={styles.ItemUser}>{item.name}</p>
+                      <p className={styles.ItemAchiev}></p>
+                      <p className={styles.ItemTime}>{formatTime(item.time)}</p>{" "}
+                    </div>
+                  ))}
+              </>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
